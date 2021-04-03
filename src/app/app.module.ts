ceaa26efdda304794505d8b1c434e76114f1572a
Miguel Vicente O. Santiago <migscay@users.noreply.github.com>
2021-04-03 15:37:32 +1100

    components part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,11 @@ import { MenuComponent } from './menu/menu.component';
 
 import { MatListModule } from '@angular/material/list';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
@@ -23,7 +28,10 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatListModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 
+const DISHES: Dish[] = [
+  {
+    name: 'Palabok',
+    image: '/assets/images/palabok.png',
+    category: 'mains',
+    label: 'Hot',
+    price: '4.99',
+    // tslint:disable-next-line:max-line-length
+    description: 'Rice noodle topped with thickened prawn bisque sauce layered with pan seared prawns sliced boiled eggs smoked fish spring onions and crushed chicharon.'
+  },
+  {
+    name: 'Kare-kare',
+    image: '/assets/images/Kare-kare.jpg',
+    category: 'mains',
+    label: '',
+    price: '1.99',
+    description: 'Crispy beef Kate kare-  slow cooked braised beef in peanut sauce with tempura aubergine,snake beans, bokchoi  And sauteed shrimp paste on the side'
+  },
+  {
+    name: 'Vadonut',
+    image: '/assets/images/vadonut.png',
+    category: 'appetizer',
+    label: 'New',
+    price: '1.99',
+    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
+  },
+  {
+    name: 'ElaiCheese Cake',
+    image: '/assets/images/elaicheesecake.png',
+    category: 'dessert',
+    label: '',
+    price: '2.99',
+    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+  }
+];
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -8,41 +44,9 @@ import { Dish } from '../shared/dish';
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = [
-    {
-      name: 'Palabok',
-      image: '/assets/images/palabok.png',
-      category: 'mains',
-      label: 'Hot',
-      price: '4.99',
-      // tslint:disable-next-line:max-line-length
-      description: 'Rice noodle topped with thickened prawn bisque sauce layered with pan seared prawns sliced boiled eggs smoked fish spring onions and crushed chicharon.'
-    },
-    {
-      name: 'Zucchipakoda',
-      image: '/assets/images/zucchipakoda.png',
-      category: 'appetizer',
-      label: '',
-      price: '1.99',
-      description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-    },
-    {
-      name: 'Vadonut',
-      image: '/assets/images/vadonut.png',
-      category: 'appetizer',
-      label: 'New',
-      price: '1.99',
-      description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-    },
-    {
-      name: 'ElaiCheese Cake',
-      image: '/assets/images/elaicheesecake.png',
-      category: 'dessert',
-      label: '',
-      price: '2.99',
-      description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-    }
-   ];
+  dishes = DISHES;
+
+  selectedDish = DISHES[0];
 
   constructor() { }
 
